08a0b201ca4e861e9091ff498271951771f39d49
Adrian Jasaroska <adrianjasaroska@gmail.com>
2024-11-14 23:28:35 +0100

    update

--- server/app.js
+++ server/app.js
@@ -1,36 +1,35 @@
 const express = require("express");
-const https = require("https");
+const http = require("http");
 const fs = require("fs");
 const { initializeAPI } = require("./api");
 
 const app = express();
 app.use(express.json());
 
-app.use(( req, res, next) => {
+app.use(( req, res, next) => { // Removes the Header which shows the Framework this Website uses
     res.removeHeader("X-Powered-By")
     next();
 })
-
+/* -> Logging Mechanism
 const logs = (req, res, next) => {
     const ignorePaths = [
-
-    ]
-    if (ignorePaths.includes(req.path)) return next();
-
-    const timestamp = new Date().toLocaleDateString();
+    ];
+    if (ignorePaths.includes(req.path)) return next(); // Skip logging
+  
+    const timestamp = new Date().toISOString();
     const user = req.user ? req.user.username : "Unauthenticated User";
+  
+    const logEntry = `[${timestamp}] User: ${user}, Method: ${req.method}, URL: ${req.originalUrl}, Status: ${res.statusCode}\n`;
+    console.log(logEntry.trim());
 
-    const logEntry = `[${timestamp}] User: ${user}, Method: ${req.method}, URL: ${req.originalURL}, Status: ${res.statusCode}\n`
-    try {
-        fs.appendFile("server_logs.log", logEntry)
-        next()    
-    } catch (error) {
-        console.error("Failed to write the log:", error)
-    }
-    
-}
+    fs.appendFile("server_logs.txt", logEntry, (err) => { //PS. fs.appendfile needs a callback function, in this case its (err)
+      if (err) console.error("Failed to write log:", err);
+    });
+  
+    next();
+  };
 app.use(logs);
-
+*/
 app.use(express.static("client"));
 // Serving the different Pages of the Frontend Views
 app.get("/login", (req, res) => {
